6d75df3cdc039b990b14832d54c3166b20fb15b6
khongkrapuns <khongkrapuns@gmail.com>
2024-03-01 18:22:34 +0700

    server js comment

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 //Route file
-/*const hotels = require('./routes/hotels');
+const hotels = require('./routes/hotels');
 const auth = require('./routes/auth');
-const bookings = require('./routes/bookings');*/
+const bookings = require('./routes/bookings');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -22,9 +22,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 //Body parser
-//app.use('/api/v1/hotels', hotels);
-//app.use('/api/v1/auth', auth);
-//app.use('/api/v1/bookings',bookings);
+app.use('/api/v1/hotels', hotels);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/bookings',bookings);
 
 const PORT=process.env.PORT || 5000;
 
