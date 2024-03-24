48b06d767d71580ad714888404f92be6c34d689d
khongkrapuns <khongkrapuns@gmail.com>
2024-03-24 19:15:51 +0700

    serverjs2

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const PORT=process.env.PORT || 5000;
 
 const server = app.listen(
     PORT,
-    console.log('Server running in ',
+    console.log('Server running in',
     process.env.NODE_ENV,
     "on " + process.env.HOST + ":" + PORT));
 
